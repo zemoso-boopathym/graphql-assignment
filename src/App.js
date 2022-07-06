99ac254bc951b8021ddf8c0e6e54158b306b434f
zemoso-boopathym <bhoopathy.muthusaamy@zemosolabs.com>
2022-07-06 17:36:33 +0530

    String cleanup into message folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import "./App.css";
-import CountryList from "./pages/CountryList";
+import './App.css';
+import CountryList from './pages/CountryList';
+import { MESSAGES } from './util/messages';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Countries</h1>
+    <div className='App'>
+      <h1>{MESSAGES.APP_NAME}</h1>
       <CountryList />
     </div>
   );
--- src/pages/CountryList.js
+++ src/pages/CountryList.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { useQuery, gql } from "@apollo/client";
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+import { MESSAGES } from '../util/messages';
 
 const GET_COUNTRIES = gql`
   query {
@@ -20,21 +21,21 @@ const GET_COUNTRIES = gql`
 function CountryList() {
   const { error, loading, data } = useQuery(GET_COUNTRIES);
 
-  if (loading) return <h1>Loading...</h1>;
+  if (loading) return <h1>{MESSAGES.LOADING}</h1>;
 
-  if (error) return <h1>Something went wrong!</h1>;
+  if (error) return <h1>{MESSAGES.SOMETHING_WRONG}</h1>;
 
   return (
     <>
-      <div className="container">
+      <div className='container'>
         {data.countries.map((country, index) => (
-          <div className="country" key={`${country.name}-${index}`}>
-            <h2>Country Name:</h2>
+          <div className='country' key={`${country.name}-${index}`}>
+            <h2>{MESSAGES.COUNTRY_NAME}</h2>
             <div>
               <div>{country.name}</div>
             </div>
 
-            <h3>Languages</h3>
+            <h3>{MESSAGES.LANGUAGES}</h3>
             <div>
               {country.languages.slice(0, 3).map((language) => (
                 <div>{language.name}</div>
